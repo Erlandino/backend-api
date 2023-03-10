56d14aaf27e4d3b04c725765613901fa16e11e8b
Erlandino <erlend.kodehode@gmail.com>
2023-03-10 11:16:35 +0100

    made the signup api check if passwords align, username is not empty and password is more than 6 characters

--- backend/middleware/verifySignUp.js
+++ backend/middleware/verifySignUp.js
@@ -3,19 +3,48 @@ const db = require("../models");
 const User = db.user;
 
 // checks if the username received from api is the same as one in the database
-function checkDuplicateUsername(req, res, next) {
+function verifySignUp(req, res, next) {
   User.findOne({ username: req.body.username }).exec((err, user) => {
     if (user) {
       if (err) {
         res.status(500).send({ message: err });
         return;
       }
-      // username is already in the database
+      // Username is already in the database
       res.status(400).send({ message: "Failed! Username is already in use!" });
       return;
     }
+
+    if (
+      !req.body.username ||
+      req.body.password.length < 6 ||
+      req.body.password !== req.body.confirmPassword
+    ) {
+      const signupErrors = {
+        username: false,
+        passwordLength: false,
+        confirmPassword: false,
+      };
+
+      // If no username is entered
+      if (!req.body.username) {
+        signupErrors.username = "No username entered";
+      }
+      // Password is less than 6 characters long
+      if (req.body.password.length < 6) {
+        signupErrors.passwordLength = "Password needs to be at least 6 characters long";
+      }
+      // First and second password inputs aren't equal
+      if (req.body.password !== req.body.confirmPassword) {
+        signupErrors.confirmPassword = "Passwords in both inputs need to be equal";
+      }
+
+      res.status(400).send(signupErrors);
+      return;
+    }
+
     next();
   });
 }
 
-module.exports = checkDuplicateUsername;
+module.exports = verifySignUp;
